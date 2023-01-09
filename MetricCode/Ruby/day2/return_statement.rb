97ec2397f3eed65e6faa4a7924033808f1dc47a5
M3tr1c_r00t <99975622+MetricCode@users.noreply.github.com>
2023-01-09 09:26:23 +0000

    Day 2 of Learning Ruby

---
+++ MetricCode/Ruby/day2/return_statement.rb
@@ -0,0 +1,6 @@
+def cube(num)
+    return num * num * num
+    5
+end
+puts cube(2)
+# any code in a function that goes after the return tag is not worked on as the return statement tells ruby that we are done with the function
