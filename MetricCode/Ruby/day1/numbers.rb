8a21eb627ab5ebcf0edcded092a098c1949e99ba
M3tr1c_r00t <99975622+MetricCode@users.noreply.github.com>
2023-01-09 09:32:41 +0000

    Day 1 of Ruby Programming

---
+++ MetricCode/Ruby/day1/numbers.rb
@@ -0,0 +1,25 @@
+#you can do basic arithmetic...
+puts 5 + 5
+
+# power....
+puts 2**3
+
+# modulus (takes the first number, divides by the second number then prints out the reminder...)
+puts 10 % 3
+
+#converts a number to a string...
+num = 10
+puts num.to_s
+# rounding
+numb1 = 10.25
+puts numb1.round()
+
+# rounding up
+puts numb1.ceil()
+
+# rounding down
+puts num.floor()
+
+
+# ruby classes Math and others...
+
